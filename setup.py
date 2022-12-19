2b58ee0d5a4cbb1169f802b4021a9e78b23fd24a
Natasha Picciani <natasha.picciani@gmail.com>
2022-12-19 13:15:35 -0500

    removed unused files

--- setup.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-from setuptools import find_packages, setup
-
-setup(
-    name="src",
-    packages=find_packages(),
-)
--- workflow/scripts/filterawaysubset.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-filter_away_subset.py hq_transcripts.fasta.no5merge.collapsed
-filter_away_subset.py Male_Med.Cogent.hq_transcripts.fasta.no5merge.collapsed
-grep -c ">" Male_Med.Cogent.hq_transcripts.fasta.no5merge.collapsed.rep.fa
-grep -c ">" Male_Med.Cogent.hq_transcripts.fasta.no5merge.collapsed.filtered.rep.fa
